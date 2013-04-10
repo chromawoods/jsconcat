30db0e941eac692b544997c2d17dfd5cff4df53b
Andreas Larsson <andreas@chromawoods.com>
2013-04-10 16:17:30 +0200

    Added directory permission check.

--- JSConcat.php
+++ JSConcat.php
@@ -5,10 +5,10 @@
  * Copyright 2013 Andreas Larsson <andreas@chromawoods.com> - http://chromawoods.com/
  * Released under the WTFPL license: http://www.wtfpl.net/
  * 
- * @version 1.0
+ * @version 1.1
  * @author Andreas Larsson
  */
- 
+
 class JSConcat {
 	
 	public static $status = "uninitialized";
@@ -33,22 +33,28 @@ class JSConcat {
     /**
      * Primary method. Concatenates JS files and minifies them.
      * 
-     * @param string $relpath Relative path where the files are located.
+     * @param string $rel_path Relative path where the files are located.
      * @param type $files Filenames, not including path but including file extension.
      * @param type $fname Name of the combined file, including file extension.
      */
-	public static function init($relpath, $files, $fname = "combined.js") {
+	public static function init($rel_path, $files, $fname = "combined.js") {
 		self::$status = "processing";
-		$relpath = trim($relpath, "/") . "/";
-		$furl = $relpath . $fname;
+        
+		$rel_path = trim($rel_path, "/") . "/";
+		$furl = $rel_path . $fname;
 		$temp_content = "";
 		$final_content = "";
-		$prepend = "/* " . $fname . " was created by jsconcat on " . date('M j Y H:i:s') . " by combining " . self::replace_last(',', ' and', implode(", ", $files)) . " */\n";
+		$prepend = "/* " . $fname . " was created by JSConcat on " . date('M j Y H:i:s') . " by combining " . self::replace_last(',', ' and', implode(", ", $files)) . " */\n";
 		
+        if (!is_writable($rel_path)) {
+            self::$status = "error";
+            die('JSConcat needs write permissions for directory ' . $rel_path);
+        }
+        
 		file_put_contents($furl, $prepend);
 		
 		foreach ($files as $f) {
-			$temp_content = file_get_contents($relpath . $f); // Fetch contents of next file
+			$temp_content = file_get_contents($rel_path . $f); // Fetch contents of next file
 			$temp_content = trim(preg_replace('/\s+/', ' ', // Remove line breaks
 				preg_replace('#\s*//.+$#m', "", // Strip single line comments
 				preg_replace('#/\*[^*]*\*+([^/][^*]*\*+)*/#', "", // Strip multi line comments
